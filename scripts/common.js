ce8e3996d25ff00c8d49bcac846820a560786667
Matus Brlit <matus.brlit@gmail.com>
2013-08-13 09:36:45 +0200

    First real commit

---
+++ scripts/common.js
@@ -0,0 +1,32 @@
+﻿Number.prototype.toString2Digits = function () {
+
+    return ("0" + this).slice(-2);
+
+}
+
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while (i--) arr[length - 1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
+function polar(base, angle, length) {
+
+    var result = new Object();
+    result.x = base.x + length * Math.cos(angle);
+    result.y = base.y + length * Math.sin(angle);
+
+    return result;
+
+}
+
+var requestAnimationFrame = window.requestAnimationFrame ||
+                            window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame ||
+                            window.msRequestAnimationFrame;
---
+++ scripts/timer.js
@@ -0,0 +1,43 @@
+﻿
+function Timer(canvasId, radius, speed, color, shade) {
+
+    var self = this;
+
+    self.shade = shade;
+    self.speed = speed;
+    self.Digits = new Array();
+    self.$clock = $("#" + canvasId);
+    var t = new Date();
+    self.Time = t.getHours().toString2Digits() + ":" + t.getMinutes().toString2Digits();
+
+    for (var i = 0; i < 5; i++) {
+        self.Digits.push(new Digit(canvasId, radius, i, speed, color, self.shade));
+    }
+
+    self.CheckTime = function () {
+
+        var t = new Date();
+        var t2 = t.getHours().toString2Digits() + ":" + t.getMinutes().toString2Digits();
+
+        if (t2 != self.Time) {
+
+            for (var s in t2) {
+                self.Digits[s].SetDigit(t2[s]);
+            }
+
+            self.Time = t2;
+
+        }
+
+        requestAnimationFrame(self.CheckTime);
+
+    };
+
+
+    for (var s in self.Time) {
+        self.Digits[s].SetDigit(self.Time[s]);
+    }
+
+    requestAnimationFrame(self.CheckTime);
+
+}
